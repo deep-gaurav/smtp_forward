6c6dd420a797f04033f77b279aaa35c2be45534b
Deep Gaurav <deepgauravraj@gmail.com>
2023-09-16 00:07:43 +0530

    skip null

--- src/schema.rs
+++ src/schema.rs
@@ -17,6 +17,7 @@ pub struct Message {
 #[serde(rename_all = "camelCase")]
 pub struct Contact {
     pub email: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>
 }
 
